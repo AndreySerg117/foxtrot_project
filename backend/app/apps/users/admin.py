126860577f32f305c1af7354e9493e847c700e2b
Andrey <Andrey827541@gmail.com>
2026-02-22 13:50:42 +0200

    create admin user and dockerize

--- backend/app/apps/users/admin.py
+++ backend/app/apps/users/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id", "first_name", "last_name",
+                    "patronymic", "document_in_passport", "nn_in_passport", "is_active", "is_staff")
+    search_fields = ("first_name", "last_name", "patronymic")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("patronymic", "document_in_passport", "nn_in_passport")}),
+    )
+
--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=12, blank=False, null=False)
-    last_name = models.CharField(max_length=24, blank=False, null=False)
-    patronymic = models.CharField(max_length=36, blank=False, null=False)
-    document_in_passport = models.CharField(max_length=24, blank=False, null=False)
-    nn_in_passport = models.CharField(max_length=24, blank=False, null=False)
+    first_name = models.CharField("Ім'я", max_length=12, blank=False, null=False)
+    last_name = models.CharField("Призвіще", max_length=24, blank=False, null=False)
+    patronymic = models.CharField("По батькові", max_length=36, blank=False, null=False)
+    document_in_passport = models.CharField("Документ пасспорта", max_length=24, blank=False, null=False)
+    nn_in_passport = models.CharField("nn пасспорта", max_length=24, blank=False, null=False)
