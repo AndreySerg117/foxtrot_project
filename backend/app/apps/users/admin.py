568372732f11b236612ce902ae18142c919e4716
Andrey <Andrey827541@gmail.com>
2026-03-01 12:41:37 +0200

    add photo in admin

--- backend/app/apps/users/admin.py
+++ backend/app/apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 
 User = get_user_model()
 
@@ -8,9 +9,21 @@ User = get_user_model()
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ("id", "first_name", "last_name",
-                    "patronymic", "document_in_passport", "nn_in_passport", "is_active", "is_staff")
+                    "patronymic", "document_in_passport", "nn_in_passport", "photo_preview")
     search_fields = ("first_name", "last_name", "patronymic")
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional info", {"fields": ("patronymic", "document_in_passport", "nn_in_passport")}),
-    )
+    fieldsets = (
+        ("Additional info", {"fields": ("patronymic", "document_in_passport",
+                                        "nn_in_passport", "photo", "photo_preview")}),
+    ) + UserAdmin.fieldsets
+    readonly_fields = ("photo_preview",)
+
+    def photo_preview(self, obj):
+        if obj.photo:
+            return format_html(
+                '<img src="{}" width="100" style="object-fit: cover; border-radius: 5px;" />',
+                obj.photo.url,
+            )
+        return "-"
+
+    photo_preview.short_description = "Photo Preview"
 
--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -9,3 +9,4 @@ class User(AbstractUser):
     patronymic = models.CharField("По батькові", max_length=36, blank=False, null=False)
     document_in_passport = models.CharField("Документ пасспорта", max_length=24, blank=False, null=False)
     nn_in_passport = models.CharField("nn пасспорта", max_length=24, blank=False, null=False)
+    photo = models.ImageField("Фото", null=True, blank=True, upload_to="user_photos/")
