a39a374a9748a4cad9efc1e7a8bf38697f75b6bd
Andrey <Andrey827541@gmail.com>
2026-03-15 19:44:44 +0200

    add details in shops

--- backend/app/apps/users/views.py
+++ backend/app/apps/users/views.py
@@ -4,11 +4,30 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from apps.users.models import User, Shop
 import logging
+from django.views.generic import DetailView
+from django.db.models import Q, prefetch_related_objects
+from django.views.generic import DetailView
+from django.contrib import messages
 
 
 @login_required(login_url='/users/login')
 def index(request):
     shops = Shop.objects.prefetch_related("sellers").all()
+    q = request.GET.get('q', "").strip()
+    if q:
+        shops = shops.filter(
+            Q(title__icontains=q)
+            | Q(sellers__first_name__icontains=q)
+            | Q(sellers__last_name__icontains=q)
+            | Q(sellers__username__icontains=q)
+        ).distinct()
+
+        if shops:
+            messages.success(request, f"Результати пошуку для запиту '{q}' ")
+        else:
+            messages.error(request, f"Нічого не знайдено для запиту '{q}' ")
+    prefetch_related_objects(shops, "sellers")
+
     return render(request, "index.html", context={"shops": shops})
 
 
@@ -52,3 +71,13 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+class ShopDetailView(DetailView):
+    model = Shop
+    template_name = "shop_detail.html"
+    context_object_name = 'shop'
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related("sellers")
+
