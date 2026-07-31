b8aec149fb35ff7045ba91416a8c4a4caa165140
Andrey <Andrey827541@gmail.com>
2026-07-31 19:44:21 +0300

    feat(auth): add Google OAuth and improve auth modal

--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=12, blank=False, null=False)
     last_name = models.CharField(max_length=24, blank=False, null=False)
-    patronymic = models.CharField(max_length=36, blank=False, null=False)
-    document_in_passport = models.CharField(max_length=24, blank=False, null=False)
-    nn_in_passport = models.CharField(max_length=24, blank=False, null=False)
+    patronymic = models.CharField(max_length=36, blank=True, null=False)
+    document_in_passport = models.CharField(max_length=24, blank=True, null=False)
+    nn_in_passport = models.CharField(max_length=24, blank=True, null=False)
     photo = models.ImageField(null=True, blank=True, upload_to="user_photos/")
     shop = models.ForeignKey(
         "Shop",
--- backend/app/apps/users/urls.py
+++ backend/app/apps/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from apps.users.views import (signup, login_view, logout_view, index, ShopDetailView, crud_users,
@@ -19,5 +19,6 @@ urlpatterns = [
     path('shops/', crud_shops, name='crud_shops'),
     path('shop/create', shop_create, name='shop_create'),
     path('shops/edit/<int:pk>/', shop_edit, name='shop_edit'),
-    path('shop/delete/<int:pk>/', shop_delete, name='shop_delete')
+    path('shop/delete/<int:pk>/', shop_delete, name='shop_delete'),
+    path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
